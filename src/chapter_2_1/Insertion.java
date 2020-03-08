cda6c4276199e719f0dbab236576863046d9c9b7
eugen57 <benke_evgenii@mail.ru>
2020-03-09 04:25:09 +0700

    Sorts: selection, insertion

---
+++ src/chapter_2_1/Insertion.java
@@ -0,0 +1,52 @@
+package chapter_2_1;
+
+import utils.StdOut;
+
+import java.util.Random;
+
+public class Insertion {
+    private static boolean less(Comparable a, Comparable b) {
+        return a.compareTo(b) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a. length; i++)
+            StdOut.print(a[i] + " ");
+        StdOut.println();
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i-1])) return false;
+        }
+        return true;
+    }
+
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        for (int i = 1; i < N; i++) {
+            for (int j = i; j > 0 && less(a[j], a[j-1]); j--) {
+                exch(a, j, j-1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] test = new Integer[10];
+        Random rand = new Random();
+        for (int i = 0; i < test.length; i++) {
+            test[i] = rand.nextInt(20);
+        }
+        System.out.println("Before sort:");
+        show(test);
+        sort(test);
+        System.out.println("After sort:");
+        show(test);
+    }
+}
---
+++ src/chapter_2_1/Selection.java
@@ -0,0 +1,56 @@
+package chapter_2_1;
+
+import utils.StdOut;
+
+import java.util.Random;
+
+public class Selection {
+    private static boolean less(Comparable a, Comparable b) {
+        return a.compareTo(b) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a. length; i++)
+            StdOut.print(a[i] + " ");
+        StdOut.println();
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i-1])) return false;
+        }
+        return true;
+    }
+
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            int min = i;
+            for (int j = i + 1; j < N; j++) {
+                if (less(a[j], a[min])) {
+                    min = j;
+                }
+            }
+            exch(a, i, min);
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] test = new Integer[10];
+        Random rand = new Random();
+        for (int i = 0; i < test.length; i++) {
+            test[i] = rand.nextInt(20);
+        }
+        System.out.println("Before sort:");
+        show(test);
+        sort(test);
+        System.out.println("After sort:");
+        show(test);
+    }
+}
