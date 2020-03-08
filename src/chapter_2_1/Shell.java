ecdfff0b9a3603e11675237d36d30cb1035ebb48
eugen57 <benke_evgenii@mail.ru>
2020-03-09 06:04:45 +0700

    Sorts: Shell

---
+++ src/chapter_2_1/Shell.java
@@ -0,0 +1,57 @@
+package chapter_2_1;
+
+import utils.StdOut;
+
+import java.util.Random;
+
+public class Shell {
+    private static boolean less(Comparable a, Comparable b) {
+        return a.compareTo(b) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a. length; i++)
+            StdOut.print(a[i] + " ");
+        StdOut.println();
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i-1])) return false;
+        }
+        return true;
+    }
+
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        int h = 1;
+        while (h < N/3) h = 3*h + 1;
+        while (h>=1) {
+            for (int i = h; i < N; i++) {
+                for (int j = i; j >= h && less(a[j], a[j-h]); j -= h) {
+                    exch(a, j, j-h);
+                }
+            }
+            h = h / 3;
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] test = new Integer[15];
+        Random rand = new Random();
+        for (int i = 0; i < test.length; i++) {
+            test[i] = rand.nextInt(20);
+        }
+        System.out.println("Before sort:");
+        show(test);
+        sort(test);
+        System.out.println("After sort:");
+        show(test);
+    }
+}
