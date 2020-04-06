ac29f85767b1ea5b171d3f2c4db27c194507ca22
Evgeny Benke <evgeny.benke@sibers.com>
2020-04-06 18:05:04 +0700

    DepthFirstPaths, BreadthFirstPaths

---
+++ src/chapter_4_1/BreadthFirstPaths.java
@@ -0,0 +1,47 @@
+package chapter_4_1;
+
+import chapter_1_3.Queue;
+import chapter_1_3.Stack;
+
+public class BreadthFirstPaths {
+    private boolean[] marked;
+    private int[] edgeTo;
+    private final int s;
+
+    public BreadthFirstPaths(Graph graph, int s) {
+        marked = new boolean[graph.V()];
+        edgeTo = new int[graph.V()];
+        this.s = s;
+        bfs(graph, s);
+    }
+
+    private void bfs(Graph graph, int s) {
+        Queue<Integer> queue = new Queue<>();
+        marked[s] = true;
+        queue.enqueue(s);
+        while (!queue.isEmpty()) {
+            int v = queue.dequeue();
+            for(int w: graph.adj(v)) {
+                if(!marked[w]) {
+                    marked[w] = true;
+                    edgeTo[w] = v;
+                    queue.enqueue(w);
+                }
+            }
+        }
+    }
+
+    public boolean hasPathTo(int v) {
+        return marked[v];
+    }
+
+    public Iterable<Integer> pathTo(int v) {
+        if (!hasPathTo(v)) return null;
+        Stack<Integer> path = new Stack<>();
+        for (int i = v; i != s; i = edgeTo[i]) {
+            path.push(i);
+        }
+        path.push(s);
+        return path;
+    }
+}
---
+++ src/chapter_4_1/DepthFirstPaths.java
@@ -0,0 +1,40 @@
+package chapter_4_1;
+
+import chapter_1_3.Stack;
+
+public class DepthFirstPaths {
+    private boolean[] marked;
+    private int[] edgeTo;
+    private final int s;
+
+    public DepthFirstPaths(Graph graph, int s) {
+        marked = new boolean[graph.V()];
+        edgeTo = new int[graph.V()];
+        this.s = s;
+        dfs(graph, s);
+    }
+
+    private void dfs(Graph graph, int v) {
+        marked[v] = true;
+        for (int w: graph.adj(v)) {
+            if(!marked[w]) {
+                edgeTo[w] = v;
+                dfs(graph, w);
+            }
+        }
+    }
+
+    public boolean hasPathTo(int v) {
+        return marked[v];
+    }
+
+    public Iterable<Integer> pathTo(int v) {
+        if(!hasPathTo(v)) return null;
+        Stack<Integer> path = new Stack<>();
+        for (int i = v; i != s; i = edgeTo[i]) {
+            path.push(i);
+        }
+        path.push(s);
+        return path;
+    }
+}
