20adfb2f122a8e2c8b70a563e05e42893da6da52
Evgeny Benke <evgeny.benke@sibers.com>
2020-04-20 21:03:22 +0700

    LazyPrimMST, PrimMST

---
+++ src/chapter_4_3/LazyPrimMST.java
@@ -0,0 +1,41 @@
+package chapter_4_3;
+
+import chapter_1_3.Queue;
+import chapter_2.PriorityQueueResize;
+
+public class LazyPrimMST {
+    private boolean[] marked;
+    private Queue<Edge> mst;
+    private PriorityQueueResize<Edge> pq;
+
+    public LazyPrimMST(EdgeWeightedGraph G) {
+        marked = new boolean[G.V()];
+        mst = new Queue<>();
+        pq = new PriorityQueueResize<>(PriorityQueueResize.Orientation.MIN);
+        visit(G, 0);
+        while (!pq.isEmpty()) {
+            Edge e = pq.deleteTop();
+            int v = e.either();
+            int w = e.other(v);
+            if(marked[v] && marked[w]) continue;
+            mst.enqueue(e);
+            if(!marked[v]) visit(G, v);
+            if(!marked[w]) visit(G, w);
+        }
+    }
+
+    private void visit(EdgeWeightedGraph G, int v) {
+        marked[v] = true;
+        for(Edge e: G.adj(v)) {
+            if(!marked[e.other(v)]) pq.insert(e);
+        }
+    }
+
+    public Iterable<Edge> edges() {
+        return mst;
+    }
+
+    public double weight() {
+        return 0.0;
+    }
+}
---
+++ src/chapter_4_3/PrimMST.java
@@ -0,0 +1,39 @@
+package chapter_4_3;
+
+import chapter_2.IndexMinPQ;
+
+public class PrimMST {
+    private Edge[] edgeTo;
+    private double[] distTo;
+    private boolean[] marked;
+    private IndexMinPQ<Double> pq;
+
+    public PrimMST(EdgeWeightedGraph G) {
+        edgeTo = new Edge[G.V()];
+        distTo = new double[G.V()];
+        for (int v = 0; v < G.V(); v++) {
+            distTo[v] = Double.POSITIVE_INFINITY;
+        }
+        marked = new boolean[G.V()];
+        pq = new IndexMinPQ<>(G.V());
+        distTo[0] = 0.0;
+        pq.insert(0, 0.0);
+        while (!pq.isEmpty()) {
+            visit(G, pq.deleteMin());
+        }
+    }
+
+    private void visit(EdgeWeightedGraph G, int v) {
+        marked[v] = true;
+        for(Edge e: G.adj(v)) {
+            int w = e.other(v);
+            if(marked[w]) continue;
+            if(e.weight() < distTo[w]) {
+                edgeTo[w] = e;
+                distTo[w] = e.weight();
+                if(pq.contains(w)) pq.changeKey(w, distTo[w]);
+                else pq.insert(w, distTo[w]);
+            }
+        }
+    }
+}
