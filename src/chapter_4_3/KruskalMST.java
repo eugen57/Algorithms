bd33cc5ef26bdb1ea8b1c4c7d168fc669826f2e9
Evgeny Benke <evgeny.benke@sibers.com>
2020-04-21 18:09:11 +0700

    KruskalMST: updates, added test-file

--- src/chapter_4_3/KruskalMST.java
+++ src/chapter_4_3/KruskalMST.java
@@ -3,6 +3,7 @@ package chapter_4_3;
 import chapter_1_3.Queue;
 import chapter_1_5.WeightedQuickUnionUF;
 import chapter_2.PriorityQueueResize;
+import utils.In;
 
 public class KruskalMST {
     private Queue<Edge> mst;
@@ -13,5 +14,35 @@ public class KruskalMST {
         for(Edge e: G.edges()) pq.insert(e);
 
         WeightedQuickUnionUF uf = new WeightedQuickUnionUF(G.V());
+        while (!pq.isEmpty() && mst.size() < G.V() - 1) {
+            Edge e = pq.deleteTop();
+            int v = e.either();
+            int w = e.other(v);
+            if(uf.connected(v, w)) continue;
+            uf.union(v, w);
+            mst.enqueue(e);
+        }
+    }
+
+    public Iterable<Edge> edges() {
+        return mst;
+    }
+
+    public double weight() {
+        double d = 0.0;
+        for(Edge e: edges()) {
+            d += e.weight();
+        }
+        return d;
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        EdgeWeightedGraph G = new EdgeWeightedGraph(in);
+        KruskalMST kMST = new KruskalMST(G);
+        for(Edge e: kMST.edges()) {
+            System.out.println(e);
+        }
+        System.out.println(kMST.weight());
     }
 }
