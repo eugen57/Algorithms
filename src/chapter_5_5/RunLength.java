5c24d1d16230e01f7406ad5df6e7e44a915572c5
Evgeny Benke <evgeny.benke@sibers.com>
2020-05-15 06:53:12 +0700

    Added RunLength compression

---
+++ src/chapter_5_5/RunLength.java
@@ -0,0 +1,46 @@
+package chapter_5_5;
+
+import utils.BinaryStdIn;
+import utils.BinaryStdOut;
+
+public class RunLength {
+    public static void expand() {
+        boolean b = false;
+        while (!BinaryStdIn.isEmpty()) {
+            char cnt = BinaryStdIn.readChar();
+            for (int i = 0; i < cnt; i++) {
+                BinaryStdOut.write(b);
+            }
+            b = !b;
+        }
+        BinaryStdOut.close();
+    }
+
+    public static void compress() {
+        char cnt = 0;
+        boolean b, old = false;
+        while (!BinaryStdIn.isEmpty()) {
+            b = BinaryStdIn.readBoolean();
+            if (b != old) {
+                BinaryStdOut.write(cnt);
+                cnt = 0;
+                old = !old;
+            } else {
+                if (cnt == 255) {
+                    BinaryStdOut.write(cnt);
+                    cnt = 0;
+                    BinaryStdOut.write(cnt);
+                }
+            }
+            cnt++;
+        }
+        BinaryStdOut.write(cnt);
+        BinaryStdOut.close();
+    }
+
+    public static void main(String[] args) {
+        if      (args[0].equals("-")) compress();
+        else if (args[0].equals("+")) expand();
+        else throw new IllegalArgumentException("Illegal command line argument");
+    }
+}
